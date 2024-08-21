d874797e3946a34b93ebd02a0318026994111e34
AvanMyers <smail1341@gmail.com>
2024-08-21 16:40:17 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string simbol;
+
+            Console.WriteLine("Введите имя:");
+            name = Console.ReadLine();
+            Console.WriteLine("Введите символ");
+            simbol = Console.ReadLine();
+
+            for (int i = 0; i < name.Length + 2; i++)
+            {
+                Console.Write(simbol);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(simbol + name + simbol);
+
+            for (int i = 0; i < name.Length + 2; i++)
+            {
+                Console.Write(simbol);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
